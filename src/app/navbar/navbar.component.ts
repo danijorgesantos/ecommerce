33156dd2b52cc07883ffa5aed285c183d7dbc5a8
Daniel Santos <daniel.santos@rupealgroup.onmicrosoft.com>
2020-07-22 17:47:35 +0100

    asd

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -33,7 +33,6 @@ export class NavbarComponent implements OnInit {
 
   ngOnInit(): void {
     this.getProducts();
-    //------------------service 
     this.authenticationService.currentMessage.pipe(debounce(() => timer(1000))).subscribe(message => {
       this.loginOn = !!this.authenticationService.currentUserValue ? true : false;
       console.log('navbar message change', this.loginOn);
